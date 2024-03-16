0672aaba5b1ee96e4a0dbe18059e21a8b3126484
eshaanmittal <eshaan.mittal1@gmail.com>
2024-03-16 20:17:30 +0100

    hits for box finished

--- VW/Objects/Box.cpp
+++ VW/Objects/Box.cpp
@@ -12,24 +12,22 @@ Box::Box(vec3 p1, vec3 p2) {
 
 bool Box::hit(Ray &ray, float tmin, float tmax) const
 {
-    float txmin = (p1[0] - ray.getOrigin()[0]) / ray.getDirection()[0];
-    float txmax = (p2[0] - ray.getOrigin()[0]) / ray.getDirection()[0];
-    float tenterx = std::min(txmin, txmax);
-    float texitx = std::max(txmin, txmax);
+    float tnear = -INFINITY;
+    float tfar = INFINITY;
 
-    float tymin = (p1[1] - ray.getOrigin()[1]) / ray.getDirection()[1];
-    float tymax = (p2[1] - ray.getOrigin()[1]) / ray.getDirection()[1];
-    float tentery = std::min(tymin, tymax);
-    float texity = std::max(tymin, tymax);
+    for (int i = 0; i < 3; i++)
+    {
+        float t0 = (p1[i] - ray.getOrigin()[i]) / ray.getDirection()[i];
+        float t1 = (p2[i] - ray.getOrigin()[i]) / ray.getDirection()[i];
+        tnear = std::max(tnear, std::min(t0, t1));
+        tfar = std::min(tfar, std::max(t0, t1));
+        if (tfar < tnear) return false;
+    }
 
-    float tenter = std::max(tenterx, tentery);
-    float texit = std::min(texitx, texity);
-    if (tenter > texit) return false;
-    if (tenter < tmin || tenter > tmax) return false;
-
+    if (tnear > tmax || tnear < tmin) return false;
     shared_ptr<HitRecord> hit = make_shared<HitRecord>();
-    hit->t = tenter;
-    hit->p = ray.pointAt(tenter);
+    hit->t = tnear;
+    hit->p = ray.pointAt(tnear);
     // Calcula la normal basada en el punt de intersecció.
     hit->normal = calculateNormal(hit->p);
     hit->mat = material;
@@ -51,42 +49,38 @@ vec3 Box::calculateNormal(const vec3 &p) const {
 bool Box::allHits(Ray &ray, float tmin, float tmax) const
 {
     bool hits = false;
-    float txmin = (p1[0] - ray.getOrigin()[0]) / ray.getDirection()[0];
-    float txmax = (p2[0] - ray.getOrigin()[0]) / ray.getDirection()[0];
-    float tenterx = std::min(txmin, txmax);
-    float texitx = std::max(txmin, txmax);
+    float tnear = -INFINITY;
+    float tfar = INFINITY;
 
-    float tymin = (p1[1] - ray.getOrigin()[1]) / ray.getDirection()[1];
-    float tymax = (p2[1] - ray.getOrigin()[1]) / ray.getDirection()[1];
-    float tentery = std::min(tymin, tymax);
-    float texity = std::max(tymin, tymax);
-
-    float tenter = std::max(tenterx, tentery);
-    float texit = std::min(texitx, texity);
-    if (tenter > texit) return false;
-    if (texit < tmin || tenter > tmax) hits = false;
-
-    if ((tenter > tmin && tenter < tmax) || (texit > tmin && texit < tmax)) 
+    for (int i = 0; i < 3; i++)
     {
+        float t0 = (p1[i] - ray.getOrigin()[i]) / ray.getDirection()[i];
+        float t1 = (p2[i] - ray.getOrigin()[i]) / ray.getDirection()[i];
+        tnear = std::max(tnear, std::min(t0, t1));
+        tfar = std::min(tfar, std::max(t0, t1));
+        if (tfar < tnear) return false;
+    }
+    
+    if ((tnear < tmax && tnear > tmin) || (tfar < tmax && tfar > tmin)) {
         shared_ptr<HitRecord> hitNear = make_shared<HitRecord>();
-        hitNear->t = tenter;
-        hitNear->p = ray.pointAt(tenter);
-        // Calcula la normal basada en el punt de intersecció.
+        hitNear->t = tnear;
+        hitNear->p = ray.pointAt(tnear);
         hitNear->normal = calculateNormal(hitNear->p);
         hitNear->mat = material;
         ray.insertHit(hitNear);
-        if (texit > tmin && texit < tmax && texit != tenter) 
-        {
+
+        if (tfar < tmax && tfar > tmin && tfar != tnear) {
             shared_ptr<HitRecord> hitFar = make_shared<HitRecord>();
-            hitFar->t = texit;
-            hitFar->p = ray.pointAt(texit);
-            // Calcula la normal basada en el punt de intersecció.
+            hitFar->t = tfar;
+            hitFar->p = ray.pointAt(tfar);
             hitFar->normal = calculateNormal(hitFar->p);
             hitFar->mat = material;
             ray.insertHit(hitFar);
         }
+
         hits = true;
     }
+
     return hits;
 }
 
